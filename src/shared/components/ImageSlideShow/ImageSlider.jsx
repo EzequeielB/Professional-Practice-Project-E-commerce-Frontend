244a5c291b4d0f958f83c5f4a96d9ab8cca6f64d
Ezequiel Beuter <ezequielbeuter@gmail.com>
2025-10-11 23:00:24 -0300

    Componente ImageSlider modificado para poder crear banners, botones y elementos para cambiar entre imagenes componetizados y css creado/COMP-03

--- src/shared/components/ImageSlideShow/ImageSlider.jsx
+++ src/shared/components/ImageSlideShow/ImageSlider.jsx
@@ -1,34 +1,50 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import Thumbnail from "./Thumbnail";
-import styles from "./ImageSlider.module.css";
+import styles from "./styles/ImageSlider.module.css";
+import stylesBanner from "./styles/BannerSlider.module.css";
+import BannerIndicators from "./BannerIndicator";
+import SliderButtons from "./SliderButtons";
 
-const ImageSlider = ({ imgs, showThumbnails = false }) => {
+const slyder = styles.sliderContainer;
+const imgSlider = styles.sliderImage;
+const bannerStyle = stylesBanner.bannerContainer;
+const bannerImg = stylesBanner.bannerImage;
+
+const ImageSlider = ({
+  imgs,
+  urls,
+  showThumbnails = false,
+  showIndicator = true,
+  banner = true,
+}) => {
   const [index, setIndex] = useState(0);
 
-  useEffect(() => {
-    setIndex(0);
-  }, [imgs]);
-
-  const next = () => setIndex(index === imgs.length - 1 ? 0 : index + 1);
-  const prev = () => setIndex(index === 0 ? imgs.length - 1 : index - 1);
-
   return (
-    <div className={styles.sliderContainer}>
-      <img src={imgs[index]} alt="slider" className={styles.sliderImage} />
-
-      <div className={styles.controls}>
-        <button className={styles.button} onClick={prev}>
-          Previous
-        </button>
-        <button className={styles.button} onClick={next}>
-          Next
-        </button>
+    <>
+      <div className={banner ? bannerStyle : slyder} onClick={() => urls && (window.location.href = urls[index])}>
+        <img
+          src={imgs[index]}
+          alt="slider"
+          className={banner ? bannerImg : imgSlider}
+        />
+        <SliderButtons
+          banner={banner}
+          imgs={imgs}
+          index={index}
+          setIndex={setIndex}
+        />
       </div>
-
       {showThumbnails && (
         <Thumbnail arr={imgs} currentIndex={index} onSelect={setIndex} />
       )}
-    </div>
+      {showIndicator && (
+        <BannerIndicators
+          imgs={imgs}
+          currentIndex={index}
+          onSelect={setIndex}
+        />
+      )}
+    </>
   );
 };
 
