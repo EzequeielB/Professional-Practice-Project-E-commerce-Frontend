30736aa9e5d309ffeeb59add8ceddb79fd00730e
Ezequiel Beuter <ezequielbeuter@gmail.com>
2025-10-14 21:01:07 -0300

    Estructura inicial Home y su config/EC-07

---
+++ src/features/home/config.js
@@ -0,0 +1,17 @@
+export const imgsBanner = [
+  "https://acdn-us.mitiendanube.com/stores/001/652/261/themes/idea/2-slide-1751818866576-2650962938-f0df0f91e3ca47b17ad57a7e33e2e7ba1751818872-1400-1400.webp?1050655741",
+];
+
+export const urlsBanner = ["http://localhost:5173/register"];
+
+export const products = [
+  {
+    url: "http://localhost:5173/register",
+    img: "https://acdn-us.mitiendanube.com/stores/001/652/261/products/img_1634-e8f54d4b2d1811358317568277391544-320-0.webp",
+    product: "x",
+    price: "x",
+    nQuotas: "x",
+    quotas: "x",
+    discount: "x",
+  },
+];
