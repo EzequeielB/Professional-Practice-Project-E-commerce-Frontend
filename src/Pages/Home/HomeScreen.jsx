bca200779bb053667eaa3fc14b8ce008c7242cb9
Ezequiel Beuter <ezequielbeuter@gmail.com>
2025-10-16 09:52:35 -0300

    Ruteo de Home/EC-07

---
+++ src/Pages/Home/HomeScreen.jsx
@@ -0,0 +1,11 @@
+import Home from "../../features/home/Home"
+
+function HomeScreen() {
+  return (
+    <>
+    <Home/>
+    </>
+  )
+}
+
+export default HomeScreen
--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -4,5 +4,6 @@ import LoginScreen from "./login/LoginScreen"
 import ResetPasswordSceen from "./resetPassword/resetPasswordScreen"
 import VerefyOTPscreen from "./verefyOTP/VerefyOTPscreen"
 import ChangePasswordScreen from "./chagePassword/ChangePasswordScreen"
+import HomeScreen from "./Home/HomeScreen"
 
-export {RegisterScreen, DashboardScreen, LoginScreen, ResetPasswordSceen, VerefyOTPscreen, ChangePasswordScreen}
+export {RegisterScreen, DashboardScreen, LoginScreen, ResetPasswordSceen, VerefyOTPscreen, ChangePasswordScreen, HomeScreen}
