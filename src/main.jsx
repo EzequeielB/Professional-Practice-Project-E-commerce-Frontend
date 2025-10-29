1245e310b62ef4c32c9a9f6f1e3457c56fefa36e
Ezequiel Beuter <ezequielbeuter@gmail.com>
2025-10-29 15:21:50 -0300

    Instalacion de react toastify y aplicado en Roles Dashboard/REVISION

--- src/main.jsx
+++ src/main.jsx
@@ -3,23 +3,13 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
 import { Router } from "./Router";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/ReactToastify.css";
 
-/** ANOTACIONES
- *
- * No es necesarios que todo este en un formulario, solo donde debes validar datos
- *
- * Cuidado con el uso del fragment (<> </>) usarlo solo si es necesario, mantener el codigo limpio
- */
-
-// TODO Cambiar los "FormButton" por el componente "Button" donde no se usen en formularios
-
-// TODO instalar react-toastify para las notificaciones emergentes: https://www.npmjs.com/package/react-toastify
-/** // TODO borrar todo lo relacionado con usePopUp y componentes de popup cuando instales react-toastify. Pero si pensas
- * que te puede servir el componente para otra funcionalidad dejalo
- */
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <RouterProvider router={Router} />
+    <ToastContainer position="top-right" autoClose={3000} />
   </StrictMode>
 );
